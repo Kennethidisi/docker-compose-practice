4e354d787a839480b46bd3ec2527cfc9f4098ed4
Kenneth Idisi <kennethidisi21@gmail.com>
2026-03-10 14:36:18 +0100

    commit 2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
-const url = "mongodb://admin:pass@mongo:27017";
+const url = "mongodb://mongo:27017";
 const client = new MongoClient(url);
 
 let db;
